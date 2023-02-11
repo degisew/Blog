2304c5a09812f73cc1671e69d287189d73b6ecdd
Degisew <jegisew21@gmail.com>
2023-02-11 21:08:03 +0300

    created controllers

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,20 @@
+class CommentsController < ApplicationController
+  def new
+    # current_user
+    @comment = Comment.new
+  end
+
+  def create
+    post_id = params[:post_id].to_i
+    post = Post.find(post_id)
+    text = params[:comment][:text]
+    @comment = Comment.new(text:, author: current_user, post:)
+    if @comment.save
+      flash[:success] = 'Comment created successfully!'
+      redirect_to user_posts_url
+    else
+      flash.now[:error] = 'Oops, Something went wrong!'
+      render :new
+    end
+  end
+end
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,9 +4,28 @@ class PostsController < ApplicationController
     @posts = Post.where(author_id: @current_user)
   end
 
+  def new
+    # current_user
+    @new_post = Post.new
+  end
+
   def show
     @current_user = User.find_by!(id: params[:user_id])
     @posts = Post.where(author_id: @current_user)
     @post = @posts.find(params[:id])
+  end
+
+  def create
+    # @post = Post.new(params.require(:new_post).permit(:title, :text))
+    title = params[:post][:title]
+    text = params[:post][:text]
+    @post = Post.new(title:, text:, comments_counter: 0, likes_counter: 0, author: current_user)
+    if @post.save
+      flash[:success] = 'Post saved successfully.'
+      redirect_to user_posts_url
+    else
+      flash.now[:error] = 'Error: Post could not be saved'
+      render :new
+    end
   end
 end
