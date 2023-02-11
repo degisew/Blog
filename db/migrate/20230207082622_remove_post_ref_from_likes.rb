31eaf280198e78f97c80cbaa78f57e5b950abb36
Degisew <jegisew21@gmail.com>
2023-02-11 22:06:05 +0300

    removed unwanted migration files

--- db/migrate/20230207082622_remove_post_ref_from_likes.rb
+++
@@ -1,5 +0,0 @@
-class RemovePostRefFromLikes < ActiveRecord::Migration[7.0]
-  def change
-    remove_reference :likes, :posts, null: false, foreign_key: true
-  end
-end
--- db/migrate/20230208102916_rename_column_posts_counter_from_user.rb
+++
@@ -1,5 +0,0 @@
-class RenameColumnPostsCounterFromUser < ActiveRecord::Migration[7.0]
-  def change
-    rename_column(:users, :posts_ounter, :posts_counter)
-  end
-end
